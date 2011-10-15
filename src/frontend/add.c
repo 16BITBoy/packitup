d6f88a21305f5a391b1b7d8e3773d215877e958f
Adrián Pérez heredia <aperezhrd@gmail.com>
2011-10-15 17:33:30 +0200

    Backup commit

--- src/frontend/add.c
+++ src/frontend/add.c
@@ -11,15 +11,20 @@ int listallfiles(char *path, PIUSTRARR *filelist){
     DIR *dirp;
     struct stat statbuf;
     struct dirent *entry;
+    static unsigned long nf = 0;
     PIUSTRING *fname = NULL;
     PIUSTRING *cwd = NULL;
-
+    PIUSTRING *dname = NULL;
+    PIUSTRARR *dirlist = newpiustrarr();
     char curitempath[PATH_MAX];
+    memset(curitempath,0, PATH_MAX);
     if((dirp = opendir(path)) == NULL){
         fprintf(stderr, "cannot open directory: %s\n", path);
+        perror("opendir");
+        printf("numero de ficheros abiertos por piuadd: %ld \n", nf);
         return 0;
     }
-    
+    nf++;
     while((entry = readdir(dirp)) != NULL){
         strcpy(curitempath, path);
         if(path[strlen(path)-1] != '/')strcat(curitempath, "/");
@@ -36,7 +41,10 @@ int listallfiles(char *path, PIUSTRARR *filelist){
             if(strcmp(entry->d_name, ".") != 0 &&
                strcmp(entry->d_name, "..") != 0)
             {
-                listallfiles(curitempath, filelist);
+                /*listallfiles(curitempath, filelist);*/
+                dname = newpiustring(PATH_MAX);
+                strcpy(dname->str, curitempath);
+                addpiustrarritem(dirlist, dname);
             }    
         }
         if(S_ISREG(statbuf.st_mode)){
@@ -45,6 +53,13 @@ int listallfiles(char *path, PIUSTRARR *filelist){
             addpiustrarritem(filelist, fname);
         }
     }
+    closedir(dirp);
+
+    unsigned long diri = 0;
+    for(diri = 0; diri < dirlist->nitems; diri++){
+        listallfiles(dirlist->items[diri].str, filelist);
+    }
+
     return 1;
 }
 
@@ -58,31 +73,25 @@ int main(int argc, char **argv){
        showhelp("Numero de argumentos incorrecto...");
        exit(1);
     }
-
     if(strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0){
         printf("piuadd - Pack It Up packager\nver:0.1.8\n\n");
         return 0;
     }
-
     if(strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0){
         showhelp(NULL);
         exit(0);
     }
-    
     if(argc < 3){
         showhelp("Se necesita un parametro mas.");
         exit(1);
     }
 
     PIUSTRARR *filelist = newpiustrarr();
-    
-
     /* Load PIU file and the file to be added */
     PIUFILE *piu = openpiufile(argv[1]);
     if(piu == NULL){
         piu = createpiufile();
     }
-    
     /* Get all files from directories if there is one as argument */
     unsigned long i;
     for(i = 2; i < argc; i++){
@@ -103,9 +112,7 @@ int main(int argc, char **argv){
         }
             
     }
-
     /* Adding files */
-    
     float prog = 0;
     printf("\nAdding new files...\n");
     for(i = 0; i < filelist->nitems; i++){
@@ -124,6 +131,5 @@ int main(int argc, char **argv){
         printf("\n%s\n",piu_errmsg(piu_errno));
         exit(3);
     }
-    
     return 0;
 }
