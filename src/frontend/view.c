1782f6197046eb813e2597b4a44ca0c5608dde7a
Adrián Pérez Heredia <aperezhrd@gmail.com>
2011-10-30 19:04:54 +0100

    Deleted some useless empty lines in 'view.c'.

--- src/frontend/view.c
+++ src/frontend/view.c
@@ -10,21 +10,16 @@ void show_help(){
 
 int main(int argc, char **argv){
     /*FIXME: Look for a way to know if the file exists*/
-
     if(argc < 2){
         show_help();
         return 1;
     }
-
     if(strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0){
         printf("\npiuview - Pack It Up file viewer\nver:0.1.2\n\n");
         return 0;
     }
-
     int file = fileopen(argv[1], 0);
-
     HEADERINFO *header = getpiuheader(file);
-    
     if(header == NULL){
         printf("%s\n", piu_errmsg(piu_errno));
         return piu_errno;
@@ -35,13 +30,12 @@ int main(int argc, char **argv){
     printf("Header size (bytes): %d\n\n", header->flistsize);
     printf("-------------Files------------------\n");
     printf("Filename\tSize\n");
-    int i;
-
+    
     if(header->filelist.filecount == 0){
         printf("\nThis file is empty\n\n");
         return 0;
     }
-
+    int i;
     for(i = 0; i < header->filelist.filecount; i++){
         printf("%s\t%ld\n",header->filelist.fileinfo[i].filename, header->filelist.fileinfo[i].size);
     }
