7d4ba36aa7f2e70c7f27b182d533336f546d6e5e
A.Perez <aperezhrd@gmail.com>
2012-05-14 22:41:15 +0200

    Some drafts of classes made.

--- src/devtests.cpp
+++ src/devtests.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <fstream>
 #include <string>
+#include <vector>
 
 #include "fdhandler/fdhandler.hpp"
 #include "tests/test_data.cpp"
@@ -9,7 +10,49 @@
 
 using namespace std;
 
+/** DRAFT: In design process **/
+
+typedef unsigned int FileListSize;
+typedef unsigned long int FileSize;
+typedef unsigned int FileNameLength;
+
+typedef struct FileInfo{
+public:
+    string fileName;
+    FileSize fileSize;
+};
+
+typedef struct PIUHeader{
+public:
+    FileListSize fileListSize;
+    vector<FileInfo> fileList;
+};
+
+// Top level class PIUArchive.
+class PIUArchive{
+private:
+    string fileName;
+    PIUHeader headerInfo;
+    vector<Data> fileData;
+public:
+    PIUArchive(string fileName);
+
+    vector<FileInfo> listFiles();
+    void write();
+    void addFile(string fileName);
+    void deleteFile(string fileName);
+    void extractFile(string fileName, string extractPath);
+};
+
+PIUArchive::PIUArchive(string fileName){
+    this->fileName = fileName;
+
+}
+/** END DRAFT **/
+
+
+
 int main(int argc, char **argv){
-    testsuite_fdhandler();
+
     return 0;
 }
