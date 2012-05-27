bd00346a46bfeca6de3c6c7a2d1c2898ea681f01
A.Perez <aperezhrd@gmail.com>
2012-05-27 02:48:35 +0200

    Solving that Eclipse IDE error was a nightmare. 3 hours thinking that my code was wrong, and all the trouble was caused by Eclipse.
    Draft classes moved to piu/piu.hpp and piu/piu.cpp for implementation code.

---
+++ src/piu/piu.cpp
@@ -0,0 +1,148 @@
+/*
+ * piu.cpp
+ *
+ *  Created on: 26/05/2012
+ *      Author: adrian
+ */
+
+#include <boost/filesystem.hpp>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+#include "piu.hpp"
+
+//namespace PIU{
+
+PIUArchive::PIUArchive(std::string fileName) throw (PIUArchiveException,
+                                               InvalidSignatureException){
+    using boost::filesystem::exists;
+    using boost::filesystem::path;
+    this->fileName = fileName;
+    path pathToPIUArchive(fileName.c_str());
+
+    if(!exists(pathToPIUArchive)){
+        return; // This will be a new file. Read nothing.
+    }
+
+    FDHandler fd(fileName);
+
+    //Check archive format signature
+    Data *inBuf = new Data();
+    fd.readchk(inBuf, 0, 3);
+    std::string signature;
+    int i;
+    char *c = NULL;
+    for(i = 0; i < 3; i++){
+        c = (char *)inBuf->data + i;
+        signature.append(1, *c);
+    }
+    if(signature.compare("PIU") != 0){
+        throw InvalidSignatureException();
+    }
+
+    //Read file list size
+    inBuf->free();
+    if(!fd.readchk(inBuf, 3, sizeof(FileListSize)))
+        throw PIUArchiveException();
+    this->headerInfo.fileListSize = *(FileListSize *)inBuf->data;
+
+    //Read the whole file list and create a pointer "buffer" pointing to the
+    //beginning of the list.
+    inBuf->free();
+    if(!fd.readchk(inBuf, 7, this->headerInfo.fileListSize))
+        throw PIUArchiveException();
+    char *buffer = (char *)inBuf->data;
+    FileListSize bytesRead = 0;
+
+    //Read the contents from the file list and store them.
+    while(bytesRead < this->headerInfo.fileListSize){
+        FileNameLength fnl = *(FileNameLength *)(buffer + bytesRead);
+        bytesRead += sizeof(FileNameLength);
+        FileInfo fi;
+        char *tmpStr = new char[fnl + 1];
+        for(i = 0; i < fnl; i++){
+            *(tmpStr + i) = *(buffer + bytesRead + i);
+        }
+        bytesRead += fnl;
+        *(tmpStr + fnl) = '\0';
+        fi.fileName = tmpStr;
+        fi.fileSize = *(FileSize *)(buffer + bytesRead);
+        bytesRead += sizeof(FileSize);
+        this->headerInfo.fileList.push_back(fi);
+        this->ops.filesKept.push_back(true);
+    }
+
+}
+
+unsigned long int PIUArchive::getFileOffset(int position){
+    FileSize offInFileDataSpace = 0;
+    FileListSize i;
+    for(i = 0; i < position; i++){
+        offInFileDataSpace += this->headerInfo.fileList[i].fileSize;
+    }
+    return PIU_SIGNATURE_SIZE + sizeof(FileListSize) + this->headerInfo.fileListSize + offInFileDataSpace;
+}
+
+// TODO
+void PIUArchive::write() throw(PIUArchiveException,
+                               PIUArchiveDoesNotExists){
+    using boost::filesystem::exists;
+    using boost::filesystem::path;
+    std::string tmpFileName = this->fileName + ".tmp";
+    FDHandler fNew(tmpFileName);
+    FDHandler fOld(this->fileName);
+    PIUArchive *oldPIUAr = NULL;
+    path oldPIUArPath(this->fileName.c_str());
+
+    /* Writing new PIU archive header
+     */
+    fNew.write((char *)PIU_SIGNATURE.c_str(), 3);
+    fNew.append((char *)&this->headerInfo.fileListSize, sizeof(FileListSize));
+    int i;
+    for(i = 0; i < this->headerInfo.fileList.size(); i++){
+        FileNameLength fnl = this->headerInfo.fileList[i].fileName.length();
+        fNew.append((char *)&fnl, sizeof(FileNameLength));
+        fNew.append((char *)this->headerInfo.fileList[i].fileName.c_str(), this->headerInfo.fileList[i].fileName.length());
+        fNew.append((char *)&this->headerInfo.fileList[i].fileSize, sizeof(FileSize));
+    }
+
+    /* Writing file data
+     */
+    Data *buf = new Data();
+    // If this is not a new PIU file, copy the files
+    // to be kept from the old version to the new one of the PIU Archive.
+    if(this->ops.filesKept.size() > 0){
+        if(!exists(oldPIUArPath))
+            throw PIUArchiveDoesNotExists();
+        oldPIUAr = new PIUArchive(this->fileName);
+        for(i=0; i<ops.filesKept.size(); i++){
+            if(ops.filesKept[i]){
+                FileSize oldOff = oldPIUAr->getFileOffset(i);
+                // DEBUG: cout << "File data is at address :" << hex << oldOff << endl;
+                if(!fOld.readchk(buf, oldOff, oldPIUAr->headerInfo.fileList[i].fileSize))
+                    throw PIUArchiveException();
+                std::cout << std::dec;
+                if(buf->size != oldPIUAr->headerInfo.fileList[i].fileSize){
+                    std::cout << "ERROR: " << std::dec << buf->size << " bytes read from old PIU file. "
+                         << headerInfo.fileList[i].fileSize - buf->size << " bytes missing."
+                         << std::endl;
+                    throw PIUArchiveException();
+                }
+                std::cout << buf->size << " bytes successfully read from old PIU file." << std::endl;
+                if(!fNew.append(buf)){
+                    throw PIUArchiveException();
+                }
+                buf->free();
+            }
+        }
+    }
+    // TODO: Write the new files into the archive.
+
+}
+
+std::vector<FileInfo> PIUArchive::listFiles(){
+    return this->headerInfo.fileList;
+}
+/** END DRAFT **/
+//} // end namespace PIU
---
+++ src/piu/piu.hpp
@@ -0,0 +1,94 @@
+/*
+ * piuarchive.hpp
+ *
+ *  Created on: 26/05/2012
+ *      Author: adrian
+ */
+#ifndef PIU_HPP_
+#define PIU_HPP_
+#include <string>
+#include <vector>
+#include "../fdhandler/fdhandler.hpp"
+
+//namespace PIU{
+typedef unsigned int FileListSize;
+typedef unsigned long int FileSize;
+typedef unsigned int FileNameLength;
+
+const unsigned int PIU_SIGNATURE_SIZE = 3;
+const std::string PIU_SIGNATURE = "PIU";
+
+class FileInfo{
+public:
+    FileInfo(){};
+    // PIU File format fields
+    std::string fileName;
+    FileSize fileSize;
+};
+
+class PIUHeader{
+public:
+    // PIU File format fields
+    FileListSize fileListSize;
+    std::vector<FileInfo> fileList;
+};
+
+class PIUArchiveException : public std::exception{
+public:
+    const char* what() const throw()
+    {
+        return "Unknown exception while handling PIU archive.";
+    }
+};
+
+class InvalidSignatureException : public PIUArchiveException{
+public:
+    const char* what() const throw()
+    {
+        return "PIU archive signature invalid.";
+    }
+};
+
+class PIUArchiveDoesNotExists : public PIUArchiveException{
+    const char* what() const throw(){
+        return "PIU archive doesn't exists.";
+    }
+};
+
+class Operations{
+public:
+    std::vector<std::string> newFiles; // New files to be written into the new PIU archive.
+    std::vector<bool> filesKept; /* Files to be written from the old PIU archive to the new one.
+                               The position of every boolean value in the vector corresponds
+                               to the position of every file in the old PIU archive.
+                            */
+};
+
+
+// Top level class PIUArchive.
+class PIUArchive{
+private:
+    std::string fileName;
+    PIUHeader headerInfo;
+    std::vector<Data> fileData;
+    Operations ops; // Operations to be done in this PIU archive.
+    unsigned long int getFileOffset(int position);
+public:
+    /** \brief Specifies the file to work with and loads the header information **/
+    PIUArchive(std::string fileName) throw(PIUArchiveException,
+                                      InvalidSignatureException);
+    /** \brief Returns a vector with the information about the files in the archive **/
+    std::vector<FileInfo> listFiles();
+    /** \brief Dumps changes to the file on disk. **/
+    void write() throw(PIUArchiveException,
+                       PIUArchiveDoesNotExists);
+    /** \brief Adds a new file to the archive in memory (just adds the header information to the file in memory) **/
+    void addFile(std::string fileName);
+    /** \brief Deletes a file from the archive in memory (just deletes the header information to the file in memory) **/
+    void deleteFile(std::string fileName);
+    /** \brief Reads the data from the file specified in the archive in memory and writes it into the specified path **/
+    void extractFile(std::string fileName, std::string extractPath);
+};
+////}
+
+#endif /* _PIU_HPP_ */
