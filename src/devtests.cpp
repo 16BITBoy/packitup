82aa07428f2628554003ecde9c16673312841a7f
Adrián Pérez Heredia <aperezhrd@gmail.com>
2013-10-20 23:02:00 +0200

    Latest changes to devtests file

--- src/devtests.cpp
+++ src/devtests.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 #include "piu/piu.hpp"
 
 using namespace PIU;
 
+void printFileInfo(FileInfo file) {
+    std::cout << file.filePath << std::endl << file.fileSize << std::endl << std::endl;
+}
 
 int main(int argc, char **argv){
     PIUArchive *ar = NULL;
     try{
-        ar = new PIUArchive("test");
-        ar->addFile("b");
+        if(argc < 3) {
+            std::cerr << "Missing parameters" << std::endl;
+            return EXIT_FAILURE;
+        }
+        ar = new PIUArchive(argv[1]);
+        ar->addFile(argv[2]);
+        std::vector<FileInfo> files = ar->listFiles();
+        std::cout << "There are : " << files.size() << " files." << std::endl;
+        for_each(files.begin(), files.end(), printFileInfo);
     }
     catch(UndefinedException e){
         std::cout << e.what() << std::endl;
     }
-
 
     return EXIT_SUCCESS;
 }
