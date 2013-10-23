8f6d43f33c346c8bf31705ce8250f3d6ba753e67
Adrián Pérez Heredia <aperezhrd@gmail.com>
2013-10-23 18:43:58 +0200

    piuadd now specifies and parses command line options, allowing to add more than one file or folder in the same call.

--- src/cli-frontend/add.cpp
+++ src/cli-frontend/add.cpp
@@ -2,10 +2,12 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <boost/program_options.hpp>
 
 #include "../piu/piu.hpp"
 
 using namespace PIU;
+namespace poptions = boost::program_options;
 
 void printFileInfo(FileInfo file) {
     std::cout << file.filePath << " : " << file.fileSize << std::endl;
@@ -14,17 +16,60 @@ void printFileInfo(FileInfo file) {
 int main(int argc, char **argv){
     PIUArchive *ar = NULL;
     try{
-        if(argc < 3) {
-            std::cerr << "Missing parameters" << std::endl;
+//        if(argc < 3) {
+//            std::cerr << "Missing parameters" << std::endl;
+//            return EXIT_FAILURE;
+//        }
+
+        poptions::options_description desc("Program options");
+        desc.add_options()
+                ("help",                                                         "Prints help information")
+                ("package,p",     poptions::value< std::string >()             , "Package file")
+                ("input-files,f", poptions::value< std::vector<std::string> >(), "Files to add to the package");
+
+        poptions::positional_options_description p;
+        p.add("input-files", -1);
+
+        poptions::variables_map vm;
+        poptions::store(poptions::command_line_parser(argc, argv).
+                  options(desc).positional(p).run(), vm);
+        poptions::notify(vm);
+
+        if(vm.count("help")) {
+            std::cout << desc << std::endl;
+            return EXIT_SUCCESS;
+        }
+
+        std::string package;
+        std::vector<std::string> files;
+
+        if(vm.count("package")) {
+            package = vm["package"].as<std::string>();
+        } else {
+            std::cerr << "Package file not specified. Use --package to specify it." << std::endl;
             return EXIT_FAILURE;
         }
-        std::string package = argv[1];
-        std::string file = argv[2];
+
+        if(vm.count("input-files")) {
+            files = vm["input-files"].as< std::vector<std::string> >();
+        } else {
+            std::cerr << "No input files specified." << std::endl;
+            return EXIT_FAILURE;
+        }
+
         ar = new PIUArchive(package);
-        ar->add(file);
-        std::vector<FileInfo> files = ar->listFiles();
-        std::cout << "There are : " << files.size() << " files." << std::endl;
-        for_each(files.begin(), files.end(), printFileInfo);
+
+        std::vector<std::string>::iterator fileIt = files.begin();
+
+        for(; fileIt != files.end(); fileIt++) {
+            std::string file = *fileIt;
+            ar->add(file);
+        }
+
+        std::vector<FileInfo> filesInside = ar->listFiles();
+        std::cout << "There are : " << filesInside.size() << " files." << std::endl;
+        for_each(filesInside.begin(), filesInside.end(), printFileInfo);
+
         ar->write();
         return EXIT_SUCCESS;
     }
